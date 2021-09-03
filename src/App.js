e0e296c5191346752c5402e41beced6a5d882b62
ProkopiakR <rafal.prokopiak@ttpsc.pl>
2021-09-03 12:11:04 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import MainRenderer from './component/main-renderer/main-renderer.js';
+
+export default function App() {
+  return (
+    <div className="App">
+      <MainRenderer />
+    </div>
+  );
+}
---
+++ src/component/main-renderer/main-renderer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MysteriesView from '../mysteries-view/mysteries-view.js';
+import './main-renderer.css';
+
+export default class MainRenderer extends React.Component {
+    render() {
+        return (
+            <div className="main-renderer">
+                <MysteriesView />
+            </div>
+        )
+    }
+}
