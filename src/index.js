769b6a0e77223f00f4d4b8cf3436d8755637e7ad
Rafał Prokopiak <rafal.prokopiak@ttpsc.pl>
2021-09-09 11:50:16 +0200

    adding favicon from real-favicon-generator

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import rose from './component/icons/rose.svg';
-import Favicon from 'react-favicon';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Favicon url={rose} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
