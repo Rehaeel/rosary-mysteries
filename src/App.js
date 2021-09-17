3537ce1bdad930bbe16cf5dc04e96ddb6c9464bd
Rafał Prokopiak <rafal.prokopiak@ttpsc.pl>
2021-09-17 16:23:19 +0200

    changing addEventListener, setting static value on vh

--- src/App.js
+++ src/App.js
@@ -41,10 +41,8 @@ export default class App extends React.Component {
   }
 
   updateVH() {
-    window.addEventListener('resize', () => {
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
   }
 
   showAlert(state) {
