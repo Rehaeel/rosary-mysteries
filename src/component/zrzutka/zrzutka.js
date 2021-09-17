2e78be2ef6dab736a26645a7c025d1e659dba8d4
Rafał Prokopiak <rafal.prokopiak@ttpsc.pl>
2021-09-17 12:16:16 +0200

    adding title to iframe

--- src/component/zrzutka/zrzutka.js
+++ src/component/zrzutka/zrzutka.js
@@ -8,9 +8,9 @@ export default class Zrzutka extends React.Component {
             <div className="zrzutka">
                 <icons.Rose className="menu-icon" alt="menu" onClick={this.props.showMenu} />
                 <iframe
+                    title="wsparcie dzieła"
                     src="https://zrzutka.pl/y6nh7c/widget/13"
                     frameborder="0" scrolling="no">
-
                 </iframe>
             </div>
         )
