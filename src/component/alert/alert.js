4780009863a1d500541ae79ae80cfdda4b414e64
Rafał Prokopiak <rafal.prokopiak@ttpsc.pl>
2021-09-17 16:52:16 +0200

    styling alert on mobile

--- src/component/alert/alert.js
+++ src/component/alert/alert.js
@@ -4,7 +4,7 @@ import './alert.css';
 export default class Alert extends React.Component {
     render() {
         return (
-            <div style={{ display: this.props.alertOverlayDisplay }} className="alert-overlay">
+            <div style={{ display: this.props.alertOverlayDisplay }} className="alert-overlay" onClick={this.props.cancelClicked}>
                 <div className="alert">
                     <h2>Czy chcesz rozpocząć od nowa?</h2>
                     <button onClick={this.props.okClicked}>Tak</button>
