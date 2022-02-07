f1322f4004a720b5c7aba025b247dd5fb784ba1c
Rehaeel <rafal@prokopiak.pl>
2022-02-07 21:11:37 +0100

    get and update starting day

--- api.js
+++ api.js
@@ -8,90 +8,45 @@ const PORT = process.env.DB_PORT;
 app.use(cors());
 app.use(express.json());
 const mystery = mysql.createConnection({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
-
-/////////// STAN
-app.get('/state', (req, res) => {
-    mystery.query('SELECT * FROM mysteriesState', (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    });
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
 });
 
 /////////// STARTING DAY
 app.get('/startingday', (req, res) => {
-    mystery.query('SELECT * FROM startingDay', (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    });
+	const { email } = req.headers;
+
+	mystery.query(
+		`SELECT startingDay FROM startingDay WHERE user='${email}'`,
+		(err, result) => {
+			if (err) {
+				res.status(404).json(err);
+			} else {
+				res.send(result[0].startingDay);
+			}
+		}
+	);
 });
 
 app.put('/startingday/:day', (req, res) => {
-    let day = req.params.day;
-    mystery.query(`UPDATE startingDay SET startingDay = '${day}' WHERE id = 1`, (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    });
+	const { day } = req.params;
+	const { email } = req.headers;
+
+	mystery.query(
+		`UPDATE startingDay SET startingDay='${day}' WHERE user='${email}'`,
+		(err, result) => {
+			if (err) {
+				res.status(404).json(err);
+			} else {
+				if (result['changedRows'] === 1) res.send('success!');
+				else res.send(result);
+			}
+		}
+	);
 });
 
-
-/////////// RADOSNE
-app.get('/radosne', (req, res) => {
-    mystery.query('SELECT * FROM mysteries WHERE part="radosne"', (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    });
-});
-
-/////////// CHWALEBNE
-app.get('/chwalebne', (req, res) => {
-    mystery.query('SELECT * FROM mysteries WHERE part="chwalebne"', (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    });
-});
-
-/////////// ŚWIATŁA
-app.get('/swiatla', (req, res) => {
-    mystery.query('SELECT * FROM mysteries WHERE part="swiatla"', (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    });
-});
-
-/////////// BOLESNE
-app.get('/bolesne', (req, res) => {
-    mystery.query('SELECT * FROM mysteries WHERE part="bolesne"', (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    });
-});
-
-app.listen(process.env.PORT || PORT, () => {
-    console.log(`My DB run on ${PORT} port`);
+app.listen(process.env.PORT, () => {
+	console.log(`My DB run on ${process.env.PORT} port`);
 });
